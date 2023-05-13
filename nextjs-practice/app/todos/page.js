93c4dd2b0226057eb396085706360cd47d35aade
mohitd427 <mohitdon0013@gmail.com>
2023-05-13 13:21:09 +0530

    displayed todos

--- nextjs-practice/app/todos/page.js
+++ nextjs-practice/app/todos/page.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 const Todos = () => {
   const [todos, setTodos] = useState("");
+  const [todo, setTodo] = useState("");
 
   const getTodos = async () => {
     try {
@@ -11,16 +12,60 @@ const Todos = () => {
       const data = await result.json();
       setTodos(data);
     } catch (error) {
-        console.log(error)
+      console.log(error);
+    }
+  };
+
+  const addTodo = async () => {
+    let todoObj = {
+      title: todo,
+      status: false,
+    };
+    try {
+      const result = await fetch("http://localhost:8080/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todoObj),
+      });
+      const data = await result.json();
+      getTodos()
+    } catch (error) {
+      console.log(error);
     }
   };
 
   useEffect(() => {
     getTodos();
+    console.log(todos, 'use Effect')
   }, []);
 
-  console.log(todos);
-  return <div>Todos</div>;
+ 
+  return (
+    <div>
+      Todos
+      <input
+        type="text"
+        placeholder="Type a Todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+
+      {
+        todos && todos.map((t) => {
+          return (
+            <div style={{ display: "flex", gap: "20px" }}>
+              <h1>Title:{t.title} </h1>
+              {t.status ? <p>Completed</p> : <p> Pending</p>}
+            </div>
+          );
+          
+        })
+      }
+    </div>
+  );
 };
 
 export default Todos;
